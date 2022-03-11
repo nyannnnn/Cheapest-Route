5f18dd2b99d6217e46d2df492a6470c9675d1647
nyannnnn <90939473+nyannnnn@users.noreply.github.com>
2022-03-11 17:52:20 -0500

    yes

--- src/i_love_recursion.java
+++ src/i_love_recursion.java
@@ -7,34 +7,31 @@ public class i_love_recursion {
 			BufferedReader in = new BufferedReader(new FileReader("input.txt"));			
 			int n = Integer.parseInt(in.readLine());
 			for(int i = 0; i < n;  i++) {
-				int x = Integer.parseInt(in.readLine());
-				int y = Integer.parseInt(in.readLine());
-				int [][] arr = new int [x][y];
-				for(int j = 0; j < x; j++) {
+				int row = Integer.parseInt(in.readLine());
+				int col = Integer.parseInt(in.readLine());
+				int [][] arr = new int [row][col];
+				for(int j = 0; j < row; j++) {
 					String [] split = in.readLine().split(" ");
-					for(int k = 0; k < y; k++) {
+					for(int k = 0; k < col; k++) {
 						arr[j][k] = Integer.parseInt(split[k]);
 					}
 				}
-				System.out.println(arr[0].length);
-				
 			}
 		}
 		catch(FileNotFoundException e) {
 			System.out.println("INVALID. File is not found");
 		}
-		
 	}
 	
-	public static void shortest (int [][] arr, int x, int y){
+	public static void shortest (int [][] arr, int row, int col){
 		
-		if(arr[0].length - 1 == x && y == 0) {
+		if(arr[0].length - 1 == row && col == 0) {
 			return;
 		}
-		if(x > arr[0].length || y > 0) {
+		if(row > arr[0].length || col > 0) {
 			return;
 		}
-		return shortest([][] arr, x+1, y-1);
+		return shortest([][] arr, row+1, col-1);
 	}
 
 }
